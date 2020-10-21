d8acbb96f342551ba30c0ff4e35cd9d37d187a47
Jude Clark <judeclark2019@gmail.com>
2020-10-20 22:42:11 -0400

    didn't do much committing this evening

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-// console.log("HELLO WORLD");
-
 $(document).ready(function () {
-  //   console.log("ready!");
-  $("#page-left").click(function () {
-    console.log("clicked page left");
-  });
-  $("#page-right").click(function () {
-    console.log("clicked page right");
+  // $("#page-left").click(function () {
+  //   console.log("clicked page left");
+  // });
+  // $("#page-right").click(function () {
+  //   console.log("clicked page right");
+  // });
+
+  // $(document).on("click", ".page-number", function () {
+  //   let clickedPage = this.dataset.page;
+  //   console.log(`clicked page number ${clickedPage}`);
+  //   if (clickedPage == 2) {
+  //     $("#port-page-1").addClass("hidden");
+  //     $("#port-page-2").removeClass("hidden");
+  //   }
+  // });
+
+  $("#about-link").click(function () {
+    console.log("clicked on about link");
+    $("#about-link").addClass("w3-text-teal");
+    $("#portfolio-link").removeClass("w3-text-teal");
+    $("#contact-link").removeClass("w3-text-teal");
   });
 
-  $(document).on("click", ".page-number", function () {
-    let clickedPage = this.dataset.page;
-    console.log(`clicked page number ${clickedPage}`);
-    if (clickedPage == 2) {
-      $("#port-page-1").addClass("hidden");
-      $("#port-page-2").removeClass("hidden");
-    }
+  $("#portfolio-link").click(function () {
+    console.log("clicked on portfolio link");
+    $("#about-link").removeClass("w3-text-teal");
+    $("#portfolio-link").addClass("w3-text-teal");
+    $("#contact-link").removeClass("w3-text-teal");
+  });
+
+  $("#contact-link").click(function () {
+    console.log("clicked on contact link");
+    $("#about-link").removeClass("w3-text-teal");
+    $("#portfolio-link").removeClass("w3-text-teal");
+    $("#contact-link").addClass("w3-text-teal");
   });
 });
